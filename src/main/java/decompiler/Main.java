6c3e8f3479ba6f848c0be7b6a3ce2a83f84728a8
Rafa <dehasi@yandex.ru>
2016-02-22 17:38:42 +0300

    initial commit

---
+++ src/main/java/decompiler/Main.java
@@ -0,0 +1,10 @@
+package decompiler;
+
+/**
+ * Created by Rafa on 22.02.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
