ab38a2f719ee49e4039a24e010d1f46f607b7b56
Rafa <dehasi@yandex.ru>
2016-02-27 19:55:55 +0300

    simple field types are parsed

--- src/main/java/decompiler/visitors/AbstractParser.java
+++ src/main/java/decompiler/visitors/AbstractParser.java
@@ -5,18 +5,18 @@ import org.objectweb.asm.Type;
 public class AbstractParser {
     public static final String DEF = "def";
 
-    private static String[] simpleTypeNames= {
-        "void",
-        "boolean",
-        "char",
-        "byte",
-        "short",
-        "int",
-        "float",
-        "long",
-        "double",
-        "array",
-        "Object"
+    private static String[] simpleTypeNames = {
+            "void",
+            "boolean",
+            "char",
+            "byte",
+            "short",
+            "int",
+            "float",
+            "long",
+            "double",
+            "array",
+            "Object"
     };
 
     protected StringBuilder parseInterfaceField(int access, String name, String desc, String signature, Object value) {
@@ -47,7 +47,7 @@ public class AbstractParser {
             if (desc.length() > 1) {
                 sb.append(parseObjName(t));
             } else {
-                sb.append(simpleTypeNames[t.getSort()]);
+                sb.append(t.getClassName());
             }
 
         }
@@ -56,61 +56,11 @@ public class AbstractParser {
 
     private StringBuilder parseObjName(Type type) {
         StringBuilder sb = new StringBuilder();
-        switch (type.getClassName().charAt(0)) {
-            case 'B':
-                sb.append("Byte");
-                break;
-            case 'C':
-                sb.append("Character");
-                break;
-            case 'D':
-                sb.append("Double");
-                break;
-            case 'L':
-                //TODO check
-                sb.append(DEF);
-                break;
-            case 'M':
-            case 'N':
-            case 'O':
-            case 'P':
-            case 'Q':
-            case 'R':
-            case 'T':
-            case 'U':
-            case 'W':
-            case 'X':
-            case 'Y':
-            default:
-                break;
-//                for (var2 = 1; var0[var1 + var2] != 59; ++var2) {
-//                    ;
-//                }
-//
-//                return new Type(10, var0, var1 + 1, var2 - 1);
-            case 'F':
-                sb.append("Float");
-                break;
-            case 'I':
-                sb.append("Integer");
-                break;
-            case 'J':
-                sb.append("Long");
-                break;
-            case 'S':
-                sb.append("Short");
-                break;
-            case 'V':
-                sb.append("Void");
-                break;
-            case 'Z':
-                sb.append("Boolean");
-                break;
-            case '[':
-//                for (var2 = 1; var0[var1 + var2] == 91; ++var2) {
-//                    ;
-//                }
+        if (type.getClassName().equals("java.lang.Object")){
+            sb.append(DEF);
+        } else {
+            sb.append(type.getClassName());
         }
-                return sb;
-        }
+        return sb;
     }
+}
