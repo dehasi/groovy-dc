02d458001f06db653311cb0c24cb09d57f5312be
Rafa <dehasi@yandex.ru>
2016-02-27 15:18:56 +0300

    small refactoring

--- src/test/java/decompiler/DecompilerTest.java
+++ src/test/java/decompiler/DecompilerTest.java
@@ -1,17 +1,12 @@
 package decompiler;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 
-/**
- * Created by Rafa on 23.02.2016.
- */
-
+@Ignore ("nothig to test")
 public class DecompilerTest {
     static final private String TESTS_DIRECTORY = "G:\\CSCLocal\\groovy-dc\\build\\classes\\main\\";
     static final private String OUTPUT_DIRECTORY = "/tmp/java";
@@ -21,22 +16,8 @@ public class DecompilerTest {
     Decompiler decompiler = new Decompiler();
 
     @Before
-    //TODO: implement compilation
     public void compileGroovyClasses() {
-        try {
-            Process process = Runtime.getRuntime().exec("groovy " + GROOVY_FILES_DIRECTORY + "*.groovy");
-
-            BufferedReader reader =
-                    new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-            String line = "";
-            StringBuilder sb = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    //TODO: implement compilation
     }
 
     @Before
@@ -51,28 +32,28 @@ public class DecompilerTest {
     @Test
     public void testEmtpyInterface() {
         Class<?> clazz = loader.loadFromDirectory("interfaces.EmptyInterface");
-        String res = decompiler.decompile(clazz);
+        StringBuilder res = decompiler.decompile(clazz);
         System.out.println(res);
     }
 
     @Test
     public void testInterfaceWithFields() {
         Class<?> clazz = loader.loadFromDirectory("interfaces.InterfaceWithFields");
-        String res = decompiler.decompile(clazz);
+        StringBuilder res = decompiler.decompile(clazz);
         System.out.println(res);
     }
 
     @Test
     public void testFullInterface() {
         Class<?> clazz = loader.loadFromDirectory("interfaces.FullInterface");
-        String res = decompiler.decompile(clazz);
+        StringBuilder res = decompiler.decompile(clazz);
         System.out.println(res);
     }
 
     @Test
     public void testGenericInterface() {
         Class<?> clazz = loader.loadFromDirectory("interfaces.GenericInterface");
-        String res = decompiler.decompile(clazz);
+        StringBuilder res = decompiler.decompile(clazz);
         System.out.println(res);
     }
 }
