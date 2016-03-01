fafe948fa6a3ae5f5aa70d8dd59722f8a11e52a9
Rafa <dehasi@yandex.ru>
2016-03-02 00:12:34 +0300

    compilator works and part of test

--- src/test/java/decompiler/TestUtils.java
+++ src/test/java/decompiler/TestUtils.java
@@ -3,6 +3,7 @@ package decompiler;
 import org.junit.BeforeClass;
 
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -15,14 +16,24 @@ import java.io.Writer;
 
 public class TestUtils {
     static final public String TESTS_DIRECTORY = "G:\\CSCLocal\\groovy-dc\\build\\classes\\main\\interfaces\\";
-    static final public String OUTPUT_DIRECTORY = "G:\\CSCLocal\\groovy-dc\\temp\\";
+    static final public String OUTPUT_DIRECTORY = "G:\\CSCLocal\\groovy-dc\\temp";
     static final public String GROOVY_FILES_DIRECTORY = "resources/test/groovy";
 
     @BeforeClass
     public static boolean compile(String path) {
         Process compileProcess = null;
+        String compiler = "groovyc";
+        String classpath = " -cp " + OUTPUT_DIRECTORY;
+
+        StringBuilder cmd = new StringBuilder("groovyc");
+        cmd.append(" -cp ")
+                .append(OUTPUT_DIRECTORY)
+                .append(" ")
+                .append(path)
+                .append(" -d ")
+                .append(OUTPUT_DIRECTORY);
         try {
-            compileProcess = Runtime.getRuntime().exec("groovyc -cp " + path);
+            compileProcess = Runtime.getRuntime().exec(cmd.toString());
             int returnCode = compileProcess.waitFor();
             PrintStream prtStrm = new PrintStream(compileProcess.getOutputStream());
             prtStrm.println();
@@ -36,10 +47,10 @@ public class TestUtils {
 
     @BeforeClass
     public static String writeDecompiledFile(String name, String content) {
-        String path = OUTPUT_DIRECTORY + name;
+        String path = OUTPUT_DIRECTORY + File.separator + name;
 
         try ( Writer out = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(path), "UTF-8"))){
+                new FileOutputStream(path)))){
             out.write(content);
         } catch (IOException e) {
             e.printStackTrace();
