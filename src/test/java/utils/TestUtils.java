fd764ffedcfae985d9f61152355192e8fe5e29d9
Rafa <dehasi@yandex.ru>
2016-03-13 00:58:33 +0300

    simple refactor: useless method was deleted

--- src/test/java/utils/TestUtils.java
+++ src/test/java/utils/TestUtils.java
@@ -9,48 +9,11 @@ import org.codehaus.groovy.tools.GroovyClass;
 import org.junit.Assert;
 import org.objectweb.asm.ClassReader;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 
 public class TestUtils {
     static final public String TESTS_DIRECTORY = "build\\classes\\main\\";
-    static final public String OUTPUT_DIRECTORY = "temp";
-
-    @Deprecated
-    public static boolean compile(String path) {
-        //TODO compile from string. file is not nessesary
-        File file = new File(path);
-        try {
-            CompilationUnit unit = new CompilationUnit();
-            unit.addSource(file);
-            unit.compile(Phases.CLASS_GENERATION);
-
-            final GroovyClass gclass = (GroovyClass) unit.getClasses().get(0);
-
-            return true;
-        } catch (CompilationFailedException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
-
-    @Deprecated
-    public static String writeDecompiledFile(String name, String content) {
-        String path = OUTPUT_DIRECTORY + File.separator + name + ".groovy";
-
-        try (Writer out = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(path)))) {
-            out.write(content);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return path;
-    }
 
     public static Class<?> convertGroovyClass2Java(GroovyClass groovyClass) {
         ByteArrayClassLoader classLoader = new ByteArrayClassLoader();
@@ -75,14 +38,12 @@ public class TestUtils {
 
     public static Class<?> loadJavaClassByName(String name) {
         ClassLoader classLoader = TestUtils.class.getClassLoader();
-
         try {
-            Class aClass = classLoader.loadClass(name);
-            System.out.println("aClass.getName() = " + aClass.getName());
-            return aClass;
+            return classLoader.loadClass(name);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
+        //TODO may be excetion is enouth?
         return null;
     }
 
@@ -90,18 +51,6 @@ public class TestUtils {
         //TODO: implement
         return true;
     }
-
-    @Deprecated
-    public static void routine(String name) throws IOException {
-        Decompiler decompiler = new Decompiler();
-        Loader loader = new Loader();
-        final String path = TESTS_DIRECTORY + "interfaces\\" + name + ".class";
-        String content = decompiler.decompile(loader.loadFromFileSystem(path)).toString();
-        String res = writeDecompiledFile(name, content);
-        System.out.println(content);
-        Assert.assertTrue(compile(res));
-    }
-
 
     public static void routine(String name, String packageName) throws IOException {
         Decompiler decompiler = new Decompiler();
