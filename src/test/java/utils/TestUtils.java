136ce2c768bc1eeb2c1a5ba11a3eb66ae76deb0c
Rafa <dehasi@yandex.ru>
2016-03-20 14:52:48 +0300

    nanes compated

--- src/test/java/utils/TestUtils.java
+++ src/test/java/utils/TestUtils.java
@@ -48,6 +48,15 @@ public class TestUtils {
     }
 
     public static boolean compareClazzes(Class<?> c1, Class<?> c2) {
+        if (c1 == null || c2 == null) {
+            return false;
+        }
+        /** compare name*/
+        if (!c1.getSimpleName().equals(c2.getSimpleName())) {return false;}
+        if (!c1.getCanonicalName().equals(c2.getCanonicalName())) {return false;}
+        if (!c1.getName().equals(c2.getName())) {return false;}
+        if (!c1.getTypeName().equals(c2.getTypeName())) {return false;}
+
         //TODO: implement
         return true;
     }
