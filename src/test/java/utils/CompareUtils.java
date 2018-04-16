708b0bf6a3dfc68c02668385d85ca7ce7926a221
Ravil Galeyev <dehasi@yandex.ru>
2018-04-16 22:25:41 +0200

    GDC-03: Add field assertion

--- src/test/java/utils/CompareUtils.java
+++ src/test/java/utils/CompareUtils.java
@@ -1,5 +1,6 @@
 package utils;
 
+import java.lang.reflect.Field;
 import java.util.Objects;
 
 import static org.junit.Assert.fail;
@@ -18,19 +19,27 @@ final class CompareUtils {
 
     private static void assertNamesEqual(Class<?> c1, Class<?> c2) {
         if (!Objects.equals(c1.getSimpleName(), c2.getSimpleName())) {
-            fail(String.format("Simple names are not equals: %s != %s", c1.getSimpleName(), c2.getSimpleName()));
+            fail(String.format("Simple names are not equal: %s != %s", c1.getSimpleName(), c2.getSimpleName()));
         }
         if (!Objects.equals(c1.getCanonicalName(), c2.getCanonicalName())) {
-            fail(String.format("Canonical names are not equals: %s != %s", c1.getCanonicalName(), c2.getCanonicalName()));
+            fail(String.format("Canonical names are not equal: %s != %s", c1.getCanonicalName(), c2.getCanonicalName()));
         }
         if (!Objects.equals(c1.getName(), c2.getName())) {
-            fail(String.format("Names are not equals: %s != %s", c1.getName(), c2.getName()));
+            fail(String.format("Names are not equal: %s != %s", c1.getName(), c2.getName()));
         }
         if (!Objects.equals(c1.getTypeName(), c2.getTypeName())) {
-            fail(String.format("Type names are not equals: %s != %s", c1.getTypeName(), c2.getTypeName()));
+            fail(String.format("Type names are not equal: %s != %s", c1.getTypeName(), c2.getTypeName()));
         }
     }
 
+    private static void assertFieldsEqual(Field f1, Field f2) {
+        if (!Objects.equals(f1.getName(), f2.getName()) ) {
+            fail(String.format("Field names are not equal: %s != %s", f1.getName(), f2.getName()));
+        }
+        if (!Objects.equals(f1.getType(), f2.getType()) ) {
+            fail(String.format("Field types are not equal: %s != %s", f1.getType(), f2.getType()));
+        }
+    }
     private CompareUtils() {
     }
 }
