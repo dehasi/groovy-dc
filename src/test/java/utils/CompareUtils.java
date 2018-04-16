94b5a5996a142f181cbf32918c77c81d675b9eed
Ravil Galeyev <dehasi@yandex.ru>
2018-04-16 20:50:15 +0200

    GDC-03: Refactor class names comparator

--- src/test/java/utils/CompareUtils.java
+++ src/test/java/utils/CompareUtils.java
@@ -1,26 +1,33 @@
 package utils;
 
-public   class CompareUtils {
+import java.util.Objects;
+
+import static org.junit.Assert.fail;
+
+public class CompareUtils {
     //TODO split to few small functions
     public static boolean compareClazzes(Class<?> c1, Class<?> c2) {
         if (c1 == null || c2 == null) {
             return false;
         }
-        /** compare name*/
-        if (!c1.getSimpleName().equals(c2.getSimpleName())) {
-            return false;
-        }
-        if (!c1.getCanonicalName().equals(c2.getCanonicalName())) {
-            return false;
-        }
-        if (!c1.getName().equals(c2.getName())) {
-            return false;
-        }
-        if (!c1.getTypeName().equals(c2.getTypeName())) {
-            return false;
-        }
+        assertNamesEqual(c1, c2);
 
         //TODO: implement
         return true;
+    }
+
+    private static void assertNamesEqual(Class<?> c1, Class<?> c2) {
+        if (!Objects.equals(c1.getSimpleName(), c2.getSimpleName())) {
+            fail(String.format("Simple names are not equals: %s != %s", c1.getSimpleName(), c2.getSimpleName()));
+        }
+        if (!Objects.equals(c1.getCanonicalName(), c2.getCanonicalName())) {
+            fail(String.format("Canonical names are not equals: %s != %s", c1.getCanonicalName(), c2.getCanonicalName()));
+        }
+        if (!Objects.equals(c1.getName(), c2.getName())) {
+            fail(String.format("Names are not equals: %s != %s", c1.getName(), c2.getName()));
+        }
+        if (!Objects.equals(c1.getTypeName(), c2.getTypeName())) {
+            fail(String.format("Type names are not equals: %s != %s", c1.getTypeName(), c2.getTypeName()));
+        }
     }
 }
